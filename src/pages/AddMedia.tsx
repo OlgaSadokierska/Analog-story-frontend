54a64e63cdde6d8cb1a300143442a1b16bd116cb
olapp1 <olapp098@gmail.com>
2024-01-13 12:48:25 +0100

    improve of add media and add products

--- src/pages/AddMedia.tsx
+++ src/pages/AddMedia.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
@@ -7,6 +7,9 @@ import Box from '@mui/material/Box';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import { PostRequests } from '../communication/network/PostRequests';
+import { GetRequests } from '../communication/network/GetRequests';
+import MenuItem from '@mui/material/MenuItem';
+import { Select } from '@mui/material';
 
 const AddMediaPage = () => {
     const [newCamera, setNewCamera] = useState({
@@ -19,7 +22,34 @@ const AddMediaPage = () => {
         loadedFrames: 0,
         isFull: false,
         isForSale: false,
+        selectedCameraId: '', // Dodajemy nowy stan, aby przechować ID wybranego aparatu
     });
+
+    const [userCameras, setUserCameras] = useState([]);
+    const [loggedInUserId, setLoggedInUserId] = useState(null); // Dodajemy stan do przechowywania ID zalogowanego użytkownika
+
+    useEffect(() => {
+        const storedUserId = localStorage.getItem('UserId');
+        if (storedUserId !== null) {
+            const parsedUserId = parseInt(storedUserId, 10);
+            setLoggedInUserId(parsedUserId);
+        }
+    }, []);
+
+    useEffect(() => {
+        const fetchUserCameras = async () => {
+            try {
+                if (loggedInUserId) {
+                    const userCamerasData = await GetRequests.getUserMedia(loggedInUserId);
+                    setUserCameras(userCamerasData.cameras);
+                }
+            } catch (error) {
+                console.error('Error fetching user cameras:', error);
+            }
+        };
+
+        fetchUserCameras();
+    }, [loggedInUserId]);
 
     const handleAddCamera = async () => {
         try {
@@ -34,7 +64,7 @@ const AddMediaPage = () => {
     const handleAddFilm = async () => {
         try {
             // Dodaj logikę wysyłania nowego filmu na serwer
-            await PostRequests.addFilm(newFilm);
+            await PostRequests.addFilm({ ...newFilm, userId: loggedInUserId }); // Dodajemy userId do danych filmu
             console.log('Dodano nowy film:', newFilm);
         } catch (error) {
             console.error('Wystąpił błąd podczas dodawania filmu:', error);
@@ -42,55 +72,96 @@ const AddMediaPage = () => {
     };
 
     return (
-        <Container component="main" maxWidth="md">
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
-                <Typography variant="h5" gutterBottom>
+        <Container component="main" maxWidth="s" sx={{ display: 'flex', justifyContent: 'center', backgroundImage: 'url(/img015.jpg)', backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh', overflow: 'hidden' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', width: '50%', alignItems: 'center', mt: 4 }}>
+                <Typography variant="h4" gutterBottom>
                     Dodaj nową kamerę
                 </Typography>
-                <TextField
-                    label="Model"
-                    variant="outlined"
-                    margin="normal"
-                    value={newCamera.model}
-                    onChange={(event) => setNewCamera((prevState) => ({ ...prevState, model: event.target.value }))}
-                />
-                <TextField
-                    label="Marka"
-                    variant="outlined"
-                    margin="normal"
-                    value={newCamera.brand}
-                    onChange={(event) => setNewCamera((prevState) => ({ ...prevState, brand: event.target.value }))}
-                />
-                <FormControlLabel
-                    control={<Checkbox checked={newCamera.isForSale} onChange={(event) => setNewCamera((prevState) => ({ ...prevState, isForSale: event.target.checked }))} />}
-                    label="Czy na sprzedaż?"
-                />
-                <Button onClick={handleAddCamera} variant="contained" sx={{ backgroundColor: '#EFC049', marginTop: '20px' }}>
-                    Dodaj kamerę
-                </Button>
+                <Box component="form" noValidate sx={{ mt: 3, p: 3, bgcolor: 'white', borderRadius: '1%', boxShadow: 1, width: '100%' }}>
+                    <div style={{ marginBottom: '10px' }}>
+                        <TextField
+                            label="Model"
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            value={newCamera.model}
+                            onChange={(event) => setNewCamera((prevState) => ({ ...prevState, model: event.target.value }))}
+                        />
+                        <TextField
+                            label="Marka"
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            value={newCamera.brand}
+                            onChange={(event) => setNewCamera((prevState) => ({ ...prevState, brand: event.target.value }))}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox checked={newCamera.isForSale} onChange={(event) => setNewCamera((prevState) => ({ ...prevState, isForSale: event.target.checked }))} />}
+                            label="Czy na sprzedaż?"
+                        />
 
-                <Typography variant="h5" gutterBottom style={{ marginTop: '40px' }}>
+                        <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px', justifyContent: 'center' }}>
+                            <Button onClick={handleAddCamera} variant="contained" sx={{ backgroundColor: '#EFC049' }}>
+                                Dodaj kamerę
+                            </Button>
+                        </div>
+                    </div>
+                </Box>
+
+                <Typography variant="h4" gutterBottom style={{ marginTop: '10px' }}>
                     Dodaj nowy film
                 </Typography>
-                <TextField
-                    label="Liczba załadowanych klatek"
-                    variant="outlined"
-                    margin="normal"
-                    type="number"
-                    value={newFilm.loadedFrames}
-                    onChange={(event) => setNewFilm((prevState) => ({ ...prevState, loadedFrames: Number(event.target.value) }))}
-                />
-                <FormControlLabel
-                    control={<Checkbox checked={newFilm.isFull} onChange={(event) => setNewFilm((prevState) => ({ ...prevState, isFull: event.target.checked }))} />}
-                    label="Czy w pełni wykorzystany?"
-                />
-                <FormControlLabel
-                    control={<Checkbox checked={newFilm.isForSale} onChange={(event) => setNewFilm((prevState) => ({ ...prevState, isForSale: event.target.checked }))} />}
-                    label="Czy na sprzedaż?"
-                />
-                <Button onClick={handleAddFilm} variant="contained" sx={{ backgroundColor: '#EFC049', marginTop: '20px' }}>
-                    Dodaj film
-                </Button>
+                <Box component="form" noValidate sx={{ mt: 3, p: 3, bgcolor: 'white', borderRadius: '1%', boxShadow: 1, width: '100%' }}>
+                    <div style={{ marginBottom: '10px' }}>
+                        <TextField
+                            label="Liczba załadowanych klatek"
+                            variant="outlined"
+                            margin="normal"
+                            type="number"
+                            fullWidth
+                            value={newFilm.loadedFrames}
+                            onChange={(event) => {
+                                // Dodajemy walidację, aby liczba załadowanych klatek nie przekraczała 32
+                                const loadedFrames = Number(event.target.value);
+                                if (loadedFrames >= 0 && loadedFrames <= 32) {
+                                    setNewFilm((prevState) => ({ ...prevState, loadedFrames }));
+                                }
+                            }}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox checked={newFilm.isFull} onChange={(event) => setNewFilm((prevState) => ({ ...prevState, isFull: event.target.checked }))} />}
+                            label="Czy w pełni wykorzystany?"
+                        />
+                        <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px', justifyContent: 'center' }}>
+                        <FormControlLabel
+                            control={<Checkbox checked={newFilm.isForSale} onChange={(event) => setNewFilm((prevState) => ({ ...prevState, isForSale: event.target.checked }))} />}
+                            label="Czy na sprzedaż?"
+                        />
+                        </div>
+
+                        <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px', justifyContent: 'center' }}>
+                            <span style={{ marginRight: '10px' }}>Powiązana kamera:</span>
+                            <Select
+                                id="related-camera"
+                                label="Related Camera"
+                                variant="outlined"
+                                value={newCamera.selectedCameraId}
+                                onChange={(e) => setNewCamera((prev) => ({ ...prev, selectedCameraId: e.target.value }))}
+                            >
+                                <MenuItem value="">Brak</MenuItem>
+                                {userCameras.map(({ brand, id, model }) => (
+                                    <MenuItem key={id} value={id} label={`${brand} ${model}`}>
+                                        {`${brand} ${model}`}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </div>
+
+                        <Button onClick={handleAddFilm} variant="contained" sx={{ backgroundColor: '#EFC049', marginTop: '10px' }}>
+                            Dodaj film
+                        </Button>
+                    </div>
+                </Box>
             </Box>
         </Container>
     );
--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
+import Container from "@mui/material/Container";
 import { PostRequests } from '../communication/network/PostRequests';
 import { GetRequests } from '../communication/network/GetRequests';
 import { ProductType, Product } from '../communication/Types';
@@ -52,55 +53,61 @@ const AddProduct = () => {
     };
 
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
-            <h1>Dodaj nowy produkt</h1>
-            <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-                <span style={{ marginRight: '10px' }}>Typ produktu:</span>
-                <Select
-                    id="product-type"
-                    label="Product Type"
-                    variant="outlined"
-                    margin="normal"
-                    value={newProduct.product_type_id}
-                    onChange={(event) => setNewProduct((prevState) => ({ ...prevState, product_type_id: event.target.value }))}
-                >
-                    {productTypes.map((productType) => (
-                        <MenuItem key={productType.id} value={productType.id} label={productType.typeName}>
-                            {productType.typeName}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </div>
-            <TextField
-                label="Opis"
-                variant="outlined"
-                margin="normal"
-                multiline
-                rows={4}
-                value={newProduct.description}
-                onChange={(event) => setNewProduct((prevState) => ({ ...prevState, description: event.target.value }))}
-            />
-            <TextField
-                label="Cena"
-                variant="outlined"
-                margin="normal"
-                type="number"
-                value={newProduct.price}
-                onChange={(event) => setNewProduct((prevState) => ({ ...prevState, price: event.target.value }))}
-                inputProps={{
-                    min: 0,
-                    step: 10.00,
-                }}
-            />
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <Button
-                onClick={handleAddProduct}
-                variant="contained"
-                sx={{ backgroundColor: '#EFC049', marginTop: '20px' }}
-            >
-                Dodaj Produkt
-            </Button>
-        </Box>
+        <Container component="main" maxWidth="s" sx={{ display: "flex", justifyContent: "center", backgroundImage: 'url(/img015.jpg)', backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh', overflow: 'hidden' }}>
+            <Box sx={{ display: "flex", flexDirection: "column", width: "50%", alignItems: "center", mt: 4 }}>
+                <h1>Dodaj nowy produkt</h1>
+                <Box component="form" noValidate sx={{ mt: 3, p: 3, bgcolor: "white", borderRadius: "1%", boxShadow: 1, }}>
+                    <div style={{ marginBottom: '20px' }}>
+                        <span style={{ marginRight: '10px' }}>Typ produktu:</span>
+                        <Select
+                            id="product-type"
+                            label="Product Type"
+                            variant="outlined"
+                            margin="normal"
+                            value={newProduct.product_type_id}
+                            onChange={(event) => setNewProduct((prevState) => ({ ...prevState, product_type_id: event.target.value }))}
+                        >
+                            {productTypes.map((productType) => (
+                                <MenuItem key={productType.id} value={productType.id} label={productType.typeName}>
+                                    {productType.typeName}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </div>
+                    <TextField
+                        label="Opis"
+                        variant="outlined"
+                        margin="normal"
+                        multiline
+                        rows={4}
+                        fullWidth
+                        value={newProduct.description}
+                        onChange={(event) => setNewProduct((prevState) => ({ ...prevState, description: event.target.value }))}
+                    />
+                    <TextField
+                        label="Cena"
+                        variant="outlined"
+                        margin="normal"
+                        type="number"
+                        fullWidth
+                        value={newProduct.price}
+                        onChange={(event) => setNewProduct((prevState) => ({ ...prevState, price: event.target.value }))}
+                        inputProps={{
+                            min: 0,
+                            step: 10.00,
+                        }}
+                    />
+                    {error && <p style={{ color: 'red', marginTop: '10px' }}>{error}</p>}
+                    <Button
+                        onClick={handleAddProduct}
+                        variant="contained"
+                        sx={{ backgroundColor: '#EFC049', marginTop: '20px' }}
+                    >
+                        Dodaj Produkt
+                    </Button>
+                </Box>
+            </Box>
+        </Container>
     );
 };
 
